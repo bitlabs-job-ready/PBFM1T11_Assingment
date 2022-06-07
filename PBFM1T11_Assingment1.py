17d40d5f667b737022b7336ba25c79e8e3b3679f
bitLabs <75418136+bitlabs-job-ready@users.noreply.github.com>
2022-06-07 17:21:15 +0530

    Add files via upload

---
+++ PBFM1T11_Assingment1.py
@@ -0,0 +1,12 @@
+"""
+Write a Python class named Student with two attributes student_id, student_name. Add a new attribute student_class. Create a function to display the entire attribute and their values in Student class.
+
+Sample Input:
+Enter Student Id : M11
+Enter Student Name: Anusha Rao
+
+Expected Output:
+Original attributes and their values of the Student class:
+Student ID: M11
+Student Name: Anusha Rao
+"""
